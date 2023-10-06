a92db1af511b03b9c8dd04d8a523c1fc7e9ee6e5
Bohdan Horetskyi <111376936+bhoretskyi@users.noreply.github.com>
2023-10-06 09:09:04 +0200

    add redux presist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,28 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 import contactReducer from './contactSlise';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+// import { createStore } from 'redux'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, contactReducer)
+
+
 
 export const store = configureStore({
   reducer: {
-    contactList: contactReducer,
+    contactList: persistedReducer ,
   },
 });
+
+// export default () => {
+//   let store = createStore(persistedReducer)
+//   let persistor = persistStore(store)
+//   return { store, persistor }
+// }
+
+export const persistor = persistStore(store)
